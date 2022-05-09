635ac7cbf318ef5f2bd129917de0093fccc376f5
ajpch <ajpchaffey@hotmail.com>
2022-05-09 15:52:51 +0100

    item controller

--- src/main/java/com/qa/ims/controller/ItemController.java
+++ src/main/java/com/qa/ims/controller/ItemController.java
@@ -1,5 +1,20 @@
 package com.qa.ims.controller;
 
-public class ItemController {
+import java.lang.System.Logger;
+import java.util.logging.LogManager;
 
+import com.qa.ims.utils.Utils;
+
+public class ItemController implements CrudController <items> {
+
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	private ItemDAO itemDAO;
+	private Utils utils;
+
+	public ItemController(ItemDAO itemDAO, Utils utils) {
+		super();
+		this.itemDAO = itemDAO;
+		this.utils = utils;
+	}
 }
